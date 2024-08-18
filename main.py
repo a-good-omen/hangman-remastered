173873cdd5e898ff7c0fae374811ece32bd6f383
a-good-omen <146872119+a-good-omen@users.noreply.github.com>
2024-08-18 20:30:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-import helper,sys
+import helper
 
 def main():
-    helper.Printer("""
-                        ╔╗─╔╗╔═══╗╔═╗─╔╗╔═══╗╔═╗╔═╗╔═══╗╔═╗─╔╗
-                        ║║─║║║╔═╗║║║╚╗║║║╔═╗║║║╚╝║║║╔═╗║║║╚╗║║
-                        ║╚═╝║║║─║║║╔╗╚╝║║║─╚╝║╔╗╔╗║║║─║║║╔╗╚╝║
-                        ║╔═╗║║╚═╝║║║╚╗║║║║╔═╗║║║║║║║╚═╝║║║╚╗║║
-                        ║║─║║║╔═╗║║║─║║║║╚╩═║║║║║║║║╔═╗║║║─║║║
-                        ╚╝─╚╝╚╝─╚╝╚╝─╚═╝╚═══╝╚╝╚╝╚╝╚╝─╚╝╚╝─╚═╝
-
+	helper.Printer("""
+	  	       _   _    _    _   _  ____ __  __    _    _   _ 
+	  	      | | | |  / \  | \ | |/ ___|  \/  |  / \  | \ | |
+		      | |_| | / _ \ |  \| | |  _| |\/| | / _ \ |  \| |
+		      |  _  |/ ___ \| |\  | |_| | |  | |/ ___ \| |\  |
+		      |_| |_/_/   \_|_| \_|\____|_|  |_/_/   \_|_| \_|
+                                                
                             ---------Created By Imu$ak----------""",delay=0.0005)
 
-    input(helper.Printer('\n\n\t\t\t\t  ↲ Press ENTER to start',delay=0.05,clear=False))
+	input(helper.Printer('\n\n\t\t\t\t  ↲ Press ENTER to start',clear=False))
 
-    helper.LoginSetup()
+	helper.LoginSetup()
 
-    helper.Printer("Loading",dots=4,repetitions=1,delay=0.05)
-
-    choice=helper.Printer("""
+	helper.Printer("Loading",dots=4,repetitions=1)
+    
+	while True:
+		helper.Printer("""
                  ##   ##  ######   ##  ##   ##  ##
                  ### ###  ##       ### ##   ##  ##
                  #######  ##       ######   ##  ##
@@ -25,22 +25,26 @@ def main():
                  ##   ##  ##       ## ###   ##  ##
                  ##   ##  ##       ##  ##   ##  ##
                  ##   ##  ######   ##  ##    ####\n\n""",delay=0.0005)
-    helper.Printer("""
+		choice=input(helper.Printer("""
                 1. Play Game                      _________
                                                            |
                 2. Game Help                               0
                                                           /|\\
                 3. View Profile                           / \\
                                                  ______________
-                4. Exit
-        """,delay=0.005,clear=False)
-    if choice=='1':
-         pass
-    elif choice=='2':
-         pass
-    elif choice=='3':
-         pass
-    elif choice=='4':
-         pass
+                4. Leaderboard
+                
+                5. Exit
+        """,delay=0.005,clear=False))
+		if choice=='1':
+			pass
+		elif choice=='2':
+		   pass
+		elif choice=='3':
+			pass
+		elif choice=='4':
+			pass
+		elif choice=='5':
+			helper.Exit()
 
 main()

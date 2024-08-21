396b1fea2cab12445a7a469bd715906a12b77419
a-good-omen <146872119+a-good-omen@users.noreply.github.com>
2024-08-21 07:32:31 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,24 +1,22 @@
 import pickle
-from helper import Printer
-
-PlayerCount=2
 
 def DataAdder(data):				#Appends new player to the database
-	global PlayerCount
 	with open("PlayerData","ab") as data_file:
 		pickle.dump(data,data_file)
 
 
 def Verifier(user,password=None):				#Helps verify player credentials
-	global PlayerCount
-	with open("PlayerData","rb") as data_file:
- 		for i in range(PlayerCount):
- 			PlayerData=pickle.load(data_file)
- 			if PlayerData['userid']==user:
- 			#	Printer("User found!")
+	from helper import Printer
+	try:
+		data_file=open("PlayerData","rb")
+		while True:
+			PlayerData=pickle.load(data_file)
+			if PlayerData['userid']==user:
  				if password:
  					Printer("Checking password",dots=3)
  					if PlayerData['passwd']==password: return True
  					else: return False
  				else: return True
- 			else: return False
+	except EOFError:
+			return False
+	finally: data_file.close()

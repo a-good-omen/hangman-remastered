66f7d519a215bc7d9e7eab69d1fb4080f4ac239e
a-good-omen <146872119+a-good-omen@users.noreply.github.com>
2024-08-25 19:10:00 +0530

    Update data.py

--- data.py
+++ data.py
@@ -2,20 +2,23 @@ import pickle,efx
 
 def DataAdder(data):				#Appends new player to the database
 	global PlayerCount
-	with open("PlayerData","ab") as data_file:
+	with open("PlayerData.dat","ab") as data_file:
 		pickle.dump(data,data_file)
 
 
 def Verifier(user,password=None):				#Helps verify player credentials
+	data_file=None
 	try:
-		data_file=open("PlayerData","rb")
+		data_file=open("PlayerData.dat","rb")
 		while True:
 			PlayerData=pickle.load(data_file)
 			if PlayerData['userid']==user:
  				if password:
  					efx.Printer("Checking password....")
- 					if PlayerData['passwd']==password: return True
- 					else: return False
- 				else: return True
+	 				if PlayerData['passwd']==password: return 'True'
+	 				else: return False
+	 			else: return 'True'
 	except EOFError: return False
-	finally: data_file.close()
+	except FileNotFoundError: return True
+	finally:
+		if data_file: data_file.close()

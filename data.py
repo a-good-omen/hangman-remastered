8a0c77893cdd2e8719d0651ca099f511867b7ef5
a-good-omen <146872119+a-good-omen@users.noreply.github.com>
2024-08-25 09:54:04 +0530

    Update data.py
    Signed-off-by: a-good-omen <146872119+a-good-omen@users.noreply.github.com>

--- data.py
+++ data.py
@@ -1,22 +1,21 @@
-import pickle
+import pickle,efx
 
 def DataAdder(data):				#Appends new player to the database
+	global PlayerCount
 	with open("PlayerData","ab") as data_file:
 		pickle.dump(data,data_file)
 
 
 def Verifier(user,password=None):				#Helps verify player credentials
-	from helper import Printer
 	try:
 		data_file=open("PlayerData","rb")
 		while True:
 			PlayerData=pickle.load(data_file)
 			if PlayerData['userid']==user:
  				if password:
- 					Printer("Checking password",dots=3)
+ 					efx.Printer("Checking password....")
  					if PlayerData['passwd']==password: return True
  					else: return False
  				else: return True
-	except EOFError:
-			return False
+	except EOFError: return False
 	finally: data_file.close()

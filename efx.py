1f56fc870faaea65dc571c897b87d8e7fcc5837c
a-good-omen <146872119+a-good-omen@users.noreply.github.com>
2024-08-28 20:38:54 +0530

    Update efx.py

--- efx.py
+++ efx.py
@@ -1,10 +1,45 @@
-import time,sys
+import time,sys,os
+
+lore="""In the late 1900s, a secluded village was haunted by a sinister figure known by the villagers as the Hangman.\nThe legend told of a dark,cursed parchment that appeared in the hands of those who crossed the Hangman’s path.\nThe parchment bore a hidden word that must be guessed to escape.
+
+For each incorrect guess, a spectral figure of the Hangman would draw closer to completion—a noose tightening\naround a ghostly neck. The cursed villagers who failed to guess the word were seized by the Hangman, their\nsouls trapped within the parchment’s dark embrace.
+
+Though the Hangman disappeared centuries ago, whispers of his curse persist. Those who find the cursed \nparchment or hear the chilling challenge are said to risk becoming the Hangman’s next victims, forever bound to\nthe terror of his eternal game.
+
+**This game has been adapted from this lore."""
+
+diffics_display="""⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣧⠀⢀                                                                  ⡠⠄⠀⠀⠄⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣿⣿⠃⠻⣧⠈⣦                                                                ⠊⢀⣤⣞⣔⢤⡈⠆
+⠀⠀⠀⠀⠀⠀⠀⠤⠴⠶⠿⢿⣿⡿⠁⠀⠀⠈⠆⠘⣷⡄                                                            ⠄⢠⣿⣧⣿⣿⣟⠇⠈⠢⡀
+⠀⠀⣤⣤⣤⣤⣤⣤⠤⠆⠂⣸⠟⠀⢀⣐⠒⠶⢶⣤⣽⣿⣦⣤⣤⣤⣀⣀⣀⠀			 ⣀⣀                            ⠌⠀⣿⠿⠿⠿⠿⣿⡀⢰⠒⠊
+⠀⠀⠈⠛⢿⣿⣷⡄⠀⠀⢠⠋⢠⣾⡟⠻⣷⣄⠀⠀⠈⠉⠛⠻⢿⣿⡿⠋                   ⠰⣿⣿⣿⣿⠆                        ⠌⢀⡾⠁⠀⠀⠀⠀⡈⣇⠀⢆
+⠀⠀⠀⠀⠀⠙⢻⣿⣦⠀⠁⢠⣿⣿⠁⠀⣿⣿⣆⠀⣇⠀⠀⣴⠟⠁                  ⢀⣀⣠⣇⣙⣻⣟⣋⣸⣄⣀⣀                  ⣀⠄⠃⠘⠇⠀⠈⠀⠀⠉⠀⣸⠆⡈⠢⢀
+⠀⠀⠀⠀⠀⠀⢸⣿⠹⣷⡄⠸⣿⣿⡄⠀⣿⣿⠏⠀⣿⡀⠊⢀⡴⠃                  ⠙⠟⣿⣿⠋⠋⠙⠙⣿⣿⠻⠋                 ⠸⡀⢰⣎⠀⠀⠀⠀⠀⢀⡴⠎⠁⠀⢐⠄⠀⡕
+⠀⠀⠀⠀⠀⠀⢸⡇⢰⠀⠙⢆⠈⠻⢷⡴⠟⠋⠀⠀⣿⣷⣾⠟                    ⣠⣴⢿⣿⠺⣶⣷⠗⣿⡿⣧⣄                   ⠑⠀⠪⡢⡀⠀⠀⠀⠈⠀⣀⢀⡴⠅⠀⠈
+⠀⠀⠀⠀⠀⠀⠸⠀⣸⠀⠀⠀⢀⣀⣠⣤⠶⠊⠁⠀⢹⣿⡃                            ⠈⠁                           ⢄⠑⠯⢞⣪⡀⣴⣾⡗⠋⠀⡀
+⠀⠀⠀⠀⠀⠀⠀⢠⣿⣴⣶⣿⣿⣿⣭⡀⠂⢤⣄⣀⢸⣿⣇                                                          ⠑⠂⠄⡈⠙⠈⢁⡠⠔⠂
+⠀⠀⠀⠀⠀⠀⠀⣼⣿⠿⠛⠉⠀⠉⠙⠛⠲⠤⠈⠙⠿⣿⣿⡄                                                             ⠈⠀⠐⠁
+⠀⠀⠀⠀⠀⠀⠰⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠧"""
+
+parts={"head":"O","torso":"|","L.arm":"/","R.arm":"\\","L.leg":"/","R.leg":"\\","shld":"_","shoe":"_","torso2":". ."}
+hangman_display=f"""
+                ______________
+ 		|             |
+H		|             |
+A		|            {parts['shld']}{parts['head']}{parts['shld']}
+N		|           {parts['L.arm']} {parts['torso']} {parts['R.arm']}
+G		|            {parts['L.leg']} {parts['R.leg']}
+M		|
+A		|
+N		|
+ 		|
+ 	  ______|______
+	 /_____________\\ """
+
 
 def ClearScreen():				#Calling this will clear the terminal window (won't work in Python's' IDLE)
-	screen=sys.stdout
-	time.sleep(0.01)
-	screen.write("\033[2J\033[H")
-	screen.flush
+	os.system('cls' if os.name == 'nt' else 'clear')
 
      
 def Printer(text,repetitions=1,clear=True,delay=0.05):					#Deals with dynamic typing effect

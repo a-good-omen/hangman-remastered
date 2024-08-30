a142524ff12e842a9c70910481b1271ee44ca9e8
a-good-omen <146872119+a-good-omen@users.noreply.github.com>
2024-08-30 07:33:14 +0530

    Update data.py
    updated data adder

--- data.py
+++ data.py
@@ -1,37 +1,37 @@
 import pickle
 
 def DataAdder(data):				#Appends new player to the database
-	global PlayerCount
-	with open("PlayerData.dat","ab") as data_file:
-		pickle.dump(data,data_file)
+	data_file=open("PlayerData.dat","rb+")
+	try:	
+		recs=pickle.load(data_file)
+		recs.append(data)
+		recs.sort(key=lambda x: x["name"])
+		data_file.seek(0)
+		pickle.dump(recs,data_file)
+	except EOFError:
+		recs=[]; recs.append(data)
+		pickle.dump(recs,data_file)
+	finally: data_file.close()
 
 
 def Verifier(user,password=None):				#Helps verify player credentials
-	data_file=None
-
+	data_file=open("PlayerData.dat","ab+")
 	try:
-		data_file=open("PlayerData.dat","rb")
 		while True:
-			PlayerData=pickle.load(data_file)
-			if PlayerData['userid']==user:
- 				if password:
-	 				if PlayerData['passwd']==password: return 'password verified'
-	 				else: return 'incorrect password'
-	 			else: return 'user exists'
-
+			PlayersData=pickle.load(data_file)
+			for Player in PlayersData:
+				if Player["userid"]==user:
+					if password:
+						if Player["passwd"]==password: return "password verified"
+	 					else: return 'incorrect password'
+		 			else: return 'user exists'
 	except EOFError: return False
-	except FileNotFoundError: return 'FileNotFound'
-	finally:
-		if data_file: data_file.close()
-
-
+	finally: data_file.close()
+	
+	
 def DataLoader(user):
-	data_file=open("PlayerData.dat","rb")
-	try:
-		while True:
-			PlayerData=pickle.load(data_file)
-			if Verifier(user): return PlayerData
-	except EOFError: data_file.close()
+	...
+
 
 def LoadWord(user,difficulty):
 	...

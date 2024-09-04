64f99bdd5bf8a0190764f3c36ec687b0414096cb
a-good-omen <146872119+a-good-omen@users.noreply.github.com>
2024-09-04 22:14:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import efx,sides
+import efx,sides,game
 
 def main():
 	efx.Printer(\
@@ -15,38 +15,7 @@ def main():
 	sides.LoginSetup()
 
 	efx.Printer("Loading....")
-	def Menu():
-		while True:
-			choice=input(efx.Printer(
-f"""                .___  ___.  _______ .__   __.  __    __  
-                |   \/   | |   ____||  \ |  | |  |  |  |                                  ■ USER: {sides.Player} ■
-                |  \  /  | |  |__   |   \|  | |  |  |  | 
-                |  |\/|  | |   __|  |  . `  | |  |  |  | 
-                |  |  |  | |  |____ |  |\   | |  `--'  | 
-                |__|  |__| |_______||__| \__|  \______/\n\n
-                [1] Play Game                      _________
-                                                           |
-                [2] Game Help                              0
-                                                          /|\\
-                [3] View Profile                          / \\
-                                                 ______________
-                [4] Leaderboard
-                
-                [5] Exit
-                
-                CHOICE: """,delay=0.0005));choice=choice.lower()
-			if choice in ("play game",'1'):
-				efx.Printer("Loading....")
-				sides.LoadGame()
-			elif choice in ("game help",'2'):
-				...
-			elif choice in ("view profile",'3'):
-				...
-			elif choice in ("leaderboard",'4'):
-				pass
-			elif choice in ("exit",'5'):
-				choice=efx.Exit(opt='menu')
-				if choice==False: Menu()
-	Menu()
+	
+	efx.Menu()
 
 main()
